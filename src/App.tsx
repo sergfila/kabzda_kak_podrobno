7b09890844a5cdc6491a3fe72d65c2e543522491
sergfila <sergfila@live.ru>
2023-03-06 22:56:42 +0300

    first app-React

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,36 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="App">
+            Hello, samurai! Let's go!
+            <Nav/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+
+    );
+}
+
+function Nav() {
+    return (
+        <div>
+            <ul className="nav-menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Product</a></li>
+                <li><a href="#">Contacts</a></li>
+            </ul>
+        </div>
+    )
+}
+
+function Star() {
+    return (
+        <div>Star</div>
+    )
 }
 
 export default App;
